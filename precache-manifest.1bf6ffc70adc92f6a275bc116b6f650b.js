5e3233882ce3d536b7a38dec120d92aca432e403
Elina <elisonfog@gmail.com>
2020-10-24 15:33:12 +0300

    Updates

---
+++ precache-manifest.1bf6ffc70adc92f6a275bc116b6f650b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b80d62b7a148924e14061f897548f7",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e397c228d086d531974c",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.c714f9d8.chunk.css"
+  },
+  {
+    "revision": "d7d4470f277506632297",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.0e5912fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.0e5912fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e397c228d086d531974c",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.4ccb660f.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
